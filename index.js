e11b47634bc20838f951f5b3d555c8dffeb7d3f5
Njerimacharia98 <njerimacharia05@gmail.com>
2023-03-30 14:47:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (location){
+    return location <42 ? 42 -location : location -42;
+}
+
+function distanceFromHqInFeet (location) {
+    return distanceFromHqInBlocks (location) * 264
+}
+
+function distanceTravelledInFeet (start,destination) {
+    return destination >start
+    ? (destination-start)* 264
+    : (start-destination) * 264;
+}
+function calculatesFarePrice (start, destination) {
+    const distanceinfeet = distanceTravelledInFeet (start, destination);
+    if (distanceinfeet <= 400) {
+        return 0;
+    } else if (distanceinfeet<2000) {
+        return (distanceinfeet-400) *0.02;
+    } else if (distanceinfeet>2000 && distanceinfeet<2500) {
+        return 25;
+    } else if (distanceinfeet>2500){
+        return  "cannot travel that far"
+    }
+}
